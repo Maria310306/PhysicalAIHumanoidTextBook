3a1786080086b963978ee47d93a07a9143df7863
Maria Kousar <mmk310306@gmail.com>
2025-12-15 22:43:24 +0500

    Add Vercel configuration and update Docusaurus settings for deployment

--- HumanoidBookUI/docusaurus.config.js
+++ HumanoidBookUI/docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Maria310306', // Usually your GitHub org/user name.
+  projectName: 'PhysicalAIHumanoidTextBook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Maria310306/PhysicalAIHumanoidTextBook/tree/main/HumanoidBookUI/',
         },
         blog: {
           showReadingTime: true,
@@ -61,7 +61,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Maria310306/PhysicalAIHumanoidTextBook/tree/main/HumanoidBookUI/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -141,7 +141,7 @@ const config = {
               // },
               {
                 label: 'Email',
-                href: 'm.kousar.310306@gmail.com',
+                href: 'mailto:m.kousar.310306@gmail.com',
               },
             ],
           },
